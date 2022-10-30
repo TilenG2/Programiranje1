81069ceb4b6a5d33b4ac34fc6477bbe65e91bcca
Tilen_G <43862401+TilenG2@users.noreply.github.com>
2022-10-30 14:07:26 +0100

    Update DN4.py

--- Domace_Naloge/DN4.py
+++ Domace_Naloge/DN4.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 zemljevid = [
     "......",
     "..##..",
@@ -13,13 +15,28 @@ for vrstica in zemljevid:
     x = 1
     vrstica += '.'
     for c in vrstica:
-        print(c, x, y)
         if c == '#' and not block:
             x1 = x
             y1 = y
             block = True
         if block and c == '.':
+            ovire.append((x1, x-1, y))
             block = False
         x += 1
     y += 1
-print(ovire)
+
+maxx, maxy = 0, 0
+_, x, y = zip(*ovire)
+maxx = max(x)
+maxy = max(y)
+
+zemljevid2 = np.empty(shape=(maxy, maxx), dtype='str')
+zemljevid2.fill('.')
+for x1, x2, y1 in ovire:
+    zemljevid2[y1-1:y1, x1-1:x2] = "#"
+
+for array in zemljevid2: # je mogoče kak bol optimaln način za prevtorit v string?
+    vrstica = ""
+    for c in array:
+        vrstica += c
+    print(vrstica)
