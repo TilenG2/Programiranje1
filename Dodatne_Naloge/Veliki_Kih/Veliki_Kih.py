23dc78d62acc0bb125df1e7031263c5ce47a612c
Tilen_G <43862401+TilenG2@users.noreply.github.com>
2022-11-06 17:45:48 +0100

    Veliki_Kih

---
+++ Dodatne_Naloge/Veliki_Kih/Veliki_Kih.py
@@ -0,0 +1,32 @@
+from math import floor
+import numpy as np
+import matplotlib.pyplot as plt
+
+coordinates = np.loadtxt("meritve.txt", dtype=np.int64)
+slika = np.zeros((256, 256), dtype=np.uint8)
+
+rewind = 0
+flag = False
+while True:
+    for _, x, y, dx, dy in coordinates:
+
+        x = x + dx * rewind
+        y = y + dy * rewind
+
+        if not(0 <= x <= 255 and 0 <= y <= 255):
+            flag = True
+            break
+    if flag:
+        rewind -= 1
+        flag = False
+    else:
+        break
+
+for color, x, y, dx, dy in coordinates:
+
+    x = x + dx * rewind
+    y = y + dy * rewind
+
+    slika[x, y] = color
+
+plt.imsave("Pred_Kih.png", slika, cmap="afmhot")
